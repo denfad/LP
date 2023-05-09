5758c7225840460d8f0ccb0ae0ebfcf72cbbdd3d
denfad <denfad2003@mail.ru>
2023-05-09 11:20:56 +0300

    cp commit

---
+++ CP/family.pl
@@ -0,0 +1,58 @@
+parents('Caroline Bouvier','John Fitzgerald "Jack"','Jacqueline Lee Kennedy').
+parents('John Fitzgerald','John Fitzgerald "Jack"','Jacqueline Lee Kennedy').
+parents('Patrick Bouvier','John Fitzgerald "Jack"','Jacqueline Lee Kennedy').
+parents('Joseph Patrick','Joseph Patrick Sr','Rose Elizabeth').
+parents('John Fitzgerald "Jack"','Joseph Patrick Sr','Rose Elizabeth').
+parents('Rose Marie "Rosemary"','Joseph Patrick Sr','Rose Elizabeth').
+parents('Kathleen Angus "Kick"','Joseph Patrick Sr','Rose Elizabeth').
+parents('Eunice Mary','Joseph Patrick Sr','Rose Elizabeth').
+parents('Patricia Helen "Pat"','Joseph Patrick Sr','Rose Elizabeth').
+parents('Robert Francis "Bobby"','Joseph Patrick Sr','Rose Elizabeth').
+parents('Jean Ann','Joseph Patrick Sr','Rose Elizabeth').
+parents('Edward Moore "Ted"','Joseph Patrick Sr','Rose Elizabeth').
+parents('Robert Sargent "Bobby" III','Robert Sargent Jr','Eunice Mary').
+parents('Maria Owings','Robert Sargent Jr','Eunice Mary').
+parents('Timothy','Robert Sargent Jr','Eunice Mary').
+parents('Mark Kennedy','Robert Sargent Jr','Eunice Mary').
+parents('Anthony Paul Kennedy','Robert Sargent Jr','Eunice Mary').
+parents('Christopher','Peter','Patricia Helen "Pat"').
+parents('Sydney Maleia Kennedy','Peter','Patricia Helen "Pat"').
+parents('Victoria','Peter','Patricia Helen "Pat"').
+parents('Robin Elizabeth','Peter','Patricia Helen "Pat"').
+parents('Kathleen Hartington','Robert Francis "Bobby"','Ethel').
+parents('Joseph Patrick II','Robert Francis "Bobby"','Ethel').
+parents('Robert Francis','Robert Francis "Bobby"','Ethel').
+parents('David Anthony','Robert Francis "Bobby"','Ethel').
+parents('Mary Courtney','Robert Francis "Bobby"','Ethel').
+parents('Michael L.','Robert Francis "Bobby"','Ethel').
+parents('Mary Kerry','Robert Francis "Bobby"','Ethel').
+parents('Christopher George','Robert Francis "Bobby"','Ethel').
+parents('Matthew Maxwell Taylor "Max"','Robert Francis "Bobby"','Ethel').
+parents('Douglas Harriman','Robert Francis "Bobby"','Ethel').
+parents('Rory Elizabeth Katherine','Robert Francis "Bobby"','Ethel').
+parents('William Kennedy','Stephen Edward','Jean Ann').
+parents('Stephen Jr','Stephen Edward','Jean Ann').
+parents('Amanda','Stephen Edward','Jean Ann').
+parents('Kym Maria','Stephen Edward','Jean Ann').
+parents('Kara Ann','Edward Moore "Ted"','Virginia Joan').
+parents('Edward Moore Jr','Edward Moore "Ted"','Virginia Joan').
+parents('Patrick Joseph II','Edward Moore "Ted"','Virginia Joan').
+parents('Joseph Patrick Sr','Patrick Joseph','Mary Augusta').
+parents('Francis Benedict','Patrick Joseph','Mary Augusta').
+parents('Mary Loretta','Patrick Joseph','Mary Augusta').
+parents('Margaret Louise','Patrick Joseph','Mary Augusta').
+parents('Mary L','Patrick','Bridget').
+parents('Joanna L','Patrick','Bridget').
+parents('John III','Patrick','Bridget').
+parents('Margaret','Patrick','Bridget').
+parents('Patrick Joseph','Patrick','Bridget').
+parents('Jacqueline Lee Kennedy','John Vernou III','Janet Norton').
+parents('Rose Elizabeth','John Francis "Honey Fitz"','Mary Josephine').
+
+%Добавим дополнительные наглядные примеры%
+parents('Alice Siliver', 'John Siliver', 'Natali Siliver').
+parents('Natali Siliver', 'Dickler Brown', 'Mary Brown').
+parents('George Brown', 'Dickler Brown', 'Mary Brown').
+parents('Alex Brown', 'Dickler Brown', 'Mary Brown').
+parents('Mari Wrown', 'Alex Brown', 'Candy Brown').
+parents('Venus Brown', 'George Brown', 'Brown Siliver').
